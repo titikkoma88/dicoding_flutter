8ae4f109b80115d3f9cf847067fbdf4b5da368f9
Panji Ramadhan <panji.ramadhan@hadeai.com>
2022-04-10 23:02:32 +0700

    ListView.separated

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class ScrollingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
+      body: ListView.separated(
         itemBuilder: (BuildContext context, int index) {
           return Container(
             height: 250,
@@ -40,6 +40,9 @@ class ScrollingScreen extends StatelessWidget {
               ),
             ),
           );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return Divider();
         },
         itemCount: numberList.length,
       ),
