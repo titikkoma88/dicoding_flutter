7f72d44d45504d041e49c57d822abb556147b7a8
Panji Ramadhan <panji.ramadhan@hadeai.com>
2022-04-10 23:27:31 +0700

    Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dicoding/second_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,91 +15,28 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Oswald',
         primarySwatch: Colors.blue,
       ),
-      home: ExpandedFlexiblePage(),
+      home: FirstScreen(),
     );
   }
 }
 
-class ExpandedFlexiblePage extends StatelessWidget {
+class FirstScreen extends StatelessWidget {
+  final String message = 'Hello from First Screen!';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                ExpandedWidget(),
-                FlexibleWidget(),
-              ],
-            ),
-            Row(
-              children: [
-                ExpandedWidget(),
-                ExpandedWidget(),
-              ],
-            ),
-            Row(
-              children: [
-                FlexibleWidget(),
-                FlexibleWidget(),
-              ],
-            ),
-            Row(
-              children: [
-                FlexibleWidget(),
-                ExpandedWidget(),
-              ],
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        title: Text('First Screen'),
       ),
-    );
-  }
-}
-
-class ExpandedWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.teal,
-          border: Border.all(color: Colors.white),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Text(
-            'Expanded',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class FlexibleWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Flexible(
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.tealAccent,
-          border: Border.all(color: Colors.white),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Text(
-            'Flexible',
-            style: TextStyle(
-              color: Colors.teal,
-              fontSize: 24,
-            ),
-          ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Pindah Screen'),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return SecondScreen(message);
+            }));
+          },
         ),
       ),
     );
