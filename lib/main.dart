474fc5d96f2843521a774fbec667bce16cae6de0
Panji Ramadhan <panji.ramadhan@hadeai.com>
2022-04-10 13:39:59 +0700

    Scaffold AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,23 @@ class FirstScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
+          onPressed: () {},
+        ),
         title: Text('First Screen'),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          ),
+        ],
       ),
     );
   }
