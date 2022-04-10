2cf3ba881a49842b4649759b8449534c12230cb8
Panji Ramadhan <panji.ramadhan@hadeai.com>
2022-04-10 21:44:02 +0700

    Input Widget, Radio

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class FirstScreen extends StatefulWidget {
 }
 
 class _FirstScreenState extends State<FirstScreen> {
-  bool lightOn = false;
+  String? language;
 
   @override
   Widget build(BuildContext context) {
@@ -32,20 +32,58 @@ class _FirstScreenState extends State<FirstScreen> {
       appBar: AppBar(
         title: Text('First Screen'),
       ),
-      body: Switch(
-        value: lightOn,
-        onChanged: (bool value) {
-          setState(() {
-            lightOn = value;
-          });
-
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text(lightOn ? 'Light On' : 'Light Off'),
-              duration: Duration(seconds: 1),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ListTile(
+            leading: Radio<String>(
+              value: 'Dart',
+              groupValue: language,
+              onChanged: (String? value) {
+                setState(() {
+                  language = value;
+                  showSnackbar();
+                });
+              },
             ),
-          );
-        },
+            title: Text('Dart'),
+          ),
+          ListTile(
+            leading: Radio<String>(
+              value: 'Kotlin',
+              groupValue: language,
+              onChanged: (String? value) {
+                setState(() {
+                  language = value;
+                  showSnackbar();
+                });
+              },
+            ),
+            title: Text('Kotlin'),
+          ),
+          ListTile(
+            leading: Radio<String>(
+              value: 'Swift',
+              groupValue: language,
+              onChanged: (String? value) {
+                setState(() {
+                  language = value;
+                  showSnackbar();
+                });
+              },
+            ),
+            title: Text('Swift'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void showSnackbar() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('$language selected'),
+        duration: Duration(seconds: 1),
       ),
     );
   }
