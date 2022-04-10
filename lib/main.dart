f22da4c30a9ad14d3f4f6076c832d46d6198e589
Panji Ramadhan <panji.ramadhan@hadeai.com>
2022-04-10 23:05:14 +0700

    Expanded

--- lib/main.dart
+++ lib/main.dart
@@ -14,38 +14,53 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Oswald',
         primarySwatch: Colors.blue,
       ),
-      home: ScrollingScreen(),
+      home: Rainbow(),
     );
   }
 }
 
-class ScrollingScreen extends StatelessWidget {
-  final List<int> numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
+class Rainbow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.separated(
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 250,
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              border: Border.all(color: Colors.black),
-            ),
-            child: Center(
-              child: Text(
-                '${numberList[index]}',
-                style: TextStyle(fontSize: 50),
-              ),
-            ),
-          );
-        },
-        separatorBuilder: (BuildContext context, int index) {
-          return Divider();
-        },
-        itemCount: numberList.length,
-      ),
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            color: Colors.red,
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.orange,
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.yellow,
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.green,
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: Container(
+            color: Colors.blue,
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.indigo,
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.purple,
+          ),
+        ),
+      ],
     );
   }
 }
